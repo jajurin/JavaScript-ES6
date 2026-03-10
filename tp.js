55aafd4158e63ad35f82f42b0ad8c85360c28c7c
NICOLÁS JAIR JAJURIN <48861075@ort>
2026-03-10 10:45:28 -0300

    intento del primer ejercicio

---
+++ tp.js
@@ -0,0 +1,17 @@
+function Ej1(nombre){
+
+    let resultado
+let nombreMin = nombre.toLowerCase()
+
+nombreMin= nombreMin-nombreMin[0]
+
+let nombreCom = nombre[0].toUpperCase()
+
+
+
+return resultado= nombreCom + nombreMin
+}
+
+ function mostrarResultado() {
+    console.log(Ej1("RAUL"))
+  }
